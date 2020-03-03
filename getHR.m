06e7562c32bf00203e6702037ed1462483866755
smarkoco <sean_oc@emory.edu>
2020-03-03 13:23:22 -0500

    updated getHR.m to print XX / YY successes, HR: ZZ

--- getHR.m
+++ getHR.m
@@ -1,15 +1,29 @@
-function [HR] = getHR(in,varargin)
+function [HR,trialCount] = getHR(in,varargin)
 % [HR] = getHR(in)  OR  [HR] = getHR(in,indexRange)
 % input the in parameter from analyzeTaskData, output the hitrate
 % (optional) input the indexRange for inspection [as, a, vector]
 
-allHits=[in.trials.hitTrial];
+allHitFlags = [in.trials.hitTrial];
+
+if nargin == 1 % HR calculation for entire session (all trials)
+    hitIdxs = allHitFlags(allHitFlags==1); numHits = length(hitIdxs);
+    missIdxs = allHitFlags(allHitFlags==0); numMiss = length(missIdxs);
+    trialCount = length(allHitFlags);
+    HR = numHits/trialCount;
+elseif nargin == 2 % HR calculation for subset of session (select trials)
+    inputIdxRng = varargin{1};
+    subsetHitFlags = allHitFlags(inputIdxRng);
+    hitIdxs = subsetHitFlags(subsetHitFlags==1); numHits = length(hitIdxs);
+    missIdxs = subsetHitFlags(subsetHitFlags==0); numMiss = length(missIdxs);
+    trialCount = length(subsetHitFlags);
+    HR = numHits/trialCount;
+end
 
 if nargin == 1
-    HR = length(allHits(allHits==1))/(length(allHits(allHits==0))+length(allHits(allHits==1)));
+    fprintf("Overall Hit Rates for %s:\n",in.trials(1).subject)
+    fprintf("%u / %u successful trials. Hitrate: %.4f\n",numHits,trialCount,HR)
 elseif nargin == 2
-    inputIdxRng = varargin{1};
-    subHits = allHits(inputIdxRng);
-    HR = length(subHits(subHits==1))/(length(subHits(subHits==0))+length(subHits(subHits==1)));
+    fprintf("Catch Trial Subset Hit Rates for %s:\n",in.trials(1).subject)
+    fprintf("%u / %u successful trials. Hitrate: %.4f\n",numHits,trialCount,HR)
 end
 end
